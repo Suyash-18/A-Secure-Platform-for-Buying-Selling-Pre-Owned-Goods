53eafec980ece5ed3fcc0526a07ea123970ce1db
om gawande <gawandeom11@gmail.com>
2025-11-04 09:56:08 +0530

    chore: comment out unused refresh token generation method and user model export

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -78,17 +78,17 @@ userSchema.methods.generateAccessToken = function (){
         }
     )
 }
-userSchema.methods.generateRefreshToken = function (){
-  return  jwt.sign(
-        {
-            _id:this._id,
+// userSchema.methods.generateRefreshToken = function (){
+//   return  jwt.sign(
+//         {
+//             _id:this._id,
 
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
+//         },
+//         process.env.REFRESH_TOKEN_SECRET,
+//         {
+//             expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+//         }
+//     )
+// }
 
-export const User = mongoose.model("User",userSchema)
+// export const User = mongoose.model("User",userSchema)
